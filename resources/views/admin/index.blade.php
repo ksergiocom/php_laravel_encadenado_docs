0d533e1828fe2d69a959867b9d1b6dc97bdbd2e5
ksergio <113699828+ksergiocom@users.noreply.github.com>
2026-06-21 11:04:24 +0200

    Update index.blade.php

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="p-9">
         <h1 class="text-4xl md:text-5xl mb-5">Listado de documentos</h1>
-        <a class="bg-black text-white p-2 px-5 inline-block mb-5" href="{{ route('admin.create') }}">Publicar documento</a>
+        <a class="bg-black text-white p-2 px-5 inline-block mb-7" href="{{ route('admin.create') }}">Publicar documento</a>
         <table class="text-left table-fixed w-full border-collapse">
             <thead>
                 <th class="p-2 border border-gray-400 w-1/2">Titulo</th>
@@ -17,9 +17,20 @@
                     <tr>
                         <td class="p-2 border border-gray-400 truncate w-1/2">{{ $documento->titulo }}</td>
                         <td class="p-2 text-center border border-gray-400">v. {{ $documento->version }}.0</td>
-                        <td class="p-2 text-center border border-gray-400">{{ $documento->estado }}</td>
+                        <td class="p-2 text-center border border-gray-400"><span class="capitalize border p-1 px-3 border-green-300 bg-green-100 text-green-700">{{ $documento->estado }}</span></td>
                         <td class="p-2 text-center border border-gray-400">{{ $documento->publicado_at }}</td>
-                        <td class="p-2 text-center border border-gray-400">opciones</td>
+                        <td class="p-2 text-center border border-gray-400">
+                            <details class="relative inline-block text-left">
+                                <summary class="cursor-pointer list-none select-none text-sm border p-1 px-3 inline-block">Acciones &darr;</summary>
+                                <div class="absolute right-0 z-10 mt-1 w-40 flex flex-col text-left bg-white border border-gray-400 shadow-lg">
+                                    <a class="p-2 hover:bg-gray-100" href="{{ asset('storage/' . $documento->path) }}">Descargar</a>
+                                    @if ($documento->estado == 'publicado')
+                                        <a class="p-2 hover:bg-gray-100" href="{{ route('admin.retirar', ['documento' => $documento]) }}">Retirar</a>
+                                        <a class="p-2 hover:bg-gray-100" href="{{ route('admin.edit', ['documento' => $documento]) }}">Sustituir</a>
+                                    @endif
+                                </div>
+                            </details>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
